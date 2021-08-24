adc6cb2d789608942b70a6acde44f965840769fa
LoTwT <709937065@qq.com>
2021-08-24 22:58:20 +0800

    LotteryStore first

---
+++ src/models/index.ts
@@ -0,0 +1,12 @@
+/**
+ * 奖池奖品
+ */
+export interface GoodSchema {
+    placeIndex: number  // 在转盘上的位置坐标，左上 => 右下顺时针，从 0 开始
+    goodId: number,     // 商品 id
+    goodName: string,   // 种类
+    rate: number,       // 中奖概率
+    initialNum: number, // 初始数量
+    restNum: number,    // 剩余数量
+    img: string,        // 图片地址
+}
---
+++ src/store/index.ts
@@ -0,0 +1,78 @@
+import { GoodSchema } from "~/models";
+import { v4 as uuidv4 } from "uuid"
+
+/**
+ * 模拟数据存储
+ */
+class LotteryStore {
+    // 存放奖池的容器
+    private _lotteryMap: Map<string, GoodSchema[]>
+
+    constructor() {
+        this._lotteryMap = new Map<string, GoodSchema[]>()
+    }
+
+    /**
+     * 是否有对应 id 的奖池
+     * @param { string } id 
+     * @return { boolean }
+     */
+    public has(id: string): boolean {
+        return this._lotteryMap.has(id)
+    }
+
+    /**
+     * 新增一个奖池
+     * @param { GoodSchema[] } goods 奖池奖品
+     * @returns { string } 奖池 id, 返回前端更新前端数据
+     */
+    public add(goods: GoodSchema[]): string {
+        const newLotteryId = uuidv4()
+        this._lotteryMap.set(newLotteryId, goods)
+
+        return newLotteryId
+    }
+
+    /**
+     * 根据 id 查找一个奖池
+     * @param id 奖池 id
+     * @returns { boolean }
+     */
+    public find(id: string): GoodSchema[] {
+        if (!this.has(id)) throw Error("no such lottery")
+
+        return this._lotteryMap.get(id)!
+    }
+
+    /**
+     * todo
+     * 修改奖池奖品
+     * @param id 
+     * @param good 
+     */
+    public put(id: string, good: GoodSchema) {
+        const lottery = this.find(id)
+        let prevGood = lottery.find((item) => item.goodId === good.goodId)
+
+        if (prevGood) {
+            prevGood = { ...good }
+        } else {
+            lottery.push(good)
+            lottery.sort((a, b) => a.placeIndex - b.placeIndex)
+        }
+
+    }
+
+    /**
+     * 删除一个奖池
+     * @param { string } id 
+     * @returns { boolean } 是否删除成功
+     */
+    public delete(id: string): boolean {
+        return this._lotteryMap.delete(id)
+    }
+}
+
+const lotteryStore = new LotteryStore()
+
+export default lotteryStore
